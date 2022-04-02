21a3203a7b8827b3b94840c376f16183848d4be0
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-02 15:41:30 +0530

    Inventory Management Program

---
+++ Rice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryDataManagementDemo
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesOfRice;
+        public List<TypesOfGrains> typesOfGrains;
+        public List<TypesOfWheats> typesOfWheats;
+    }
+
+
+    public class TypesOfRice
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class TypesOfGrains
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class TypesOfWheats
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
