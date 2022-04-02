0256f6ae420c2691f01a34d17cf5de4174527ee5
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-02 15:35:41 +0530

    Welcome to Object Oriented Programs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOPS
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("WelCome to Object-Oriented-Programs");
+        }
+    }
+}
